ad3ebd62d0be8edf6b28857067537702dc9cbb5e
Ygor <ygorbravimr@gmail.com>
2024-09-19 09:49:09 -0300

    feat: project base setup

---
+++ src/components/Home/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./styles.module.scss";
+
+export const HomePage = () => {
+  return (
+    <div>
+      <h1>New app, bora trabalhar!</h1>
+      <p className={styles.p}>Vai dar bom!!</p>
+    </div>
+  );
+};
